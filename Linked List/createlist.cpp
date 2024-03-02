66f5ed38d6af7753c3583b4af6a4771e860eb6df
Skbarai <108645452+Skbarai@users.noreply.github.com>
2024-03-03 00:40:48 +0530

    display function added to traverse linked List

--- Linked List/createlist.cpp
+++ Linked List/createlist.cpp
@@ -7,6 +7,16 @@ class node
     node* next;
 };//this class for creating nodes which will have two fields 
 // data and pointer to next node
+void displaylist(node *ptr){
+   int counter=0;
+   while(ptr!=NULL)
+   {
+    cout<<ptr->data<<endl;
+    ptr=ptr->next;
+    counter++;
+   }
+    cout<<counter;
+}
 int main()
 {
     node *head=new node;//head is the begining of the list. This line will create a pointer to the object which points node type object. 
@@ -20,5 +30,6 @@ int main()
     third->next=NULL;
     cout<<head<<endl<<head->data<<endl<<head->next<<endl;
     cout<<second<<endl<<second->data<<endl<<head->next->data<<endl<<second->next<<endl;
-    cout<<third<<endl<<third->data<<endl<<second->next->data<<endl<<head->next->next->data<<endl<<third->next;
+    cout<<third<<endl<<third->data<<endl<<second->next->data<<endl<<head->next->next->data<<endl<<third->next<<endl;
+    displaylist(head);
 }
